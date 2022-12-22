8df8e0f4831a58c65cfb9e9666603966b01ec2a6
alexhrinyov <hrinyou29@gmail.com>
2022-12-22 22:05:33 +0300

    Add project files.

---
+++ U1(Union)/Program.cs
@@ -0,0 +1,33 @@
+﻿
+namespace U1_Union_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //UniqueSymbols();
+            UniqueSymbols2();
+        }
+
+        static void UniqueSymbols()
+        {
+            string word1 = "Каракатица";
+            string word2 = "Коромысло";
+            IEnumerable<char> union = word1.Union(word2);
+            foreach (var item in union)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        static void UniqueSymbols2()
+        {
+            Console.WriteLine("Введите строчку: ");
+            string chars = Console.ReadLine();
+            string uselessChars = " ,.;:?!";
+            var uniqChars = chars.Distinct().Except(uselessChars).ToArray();
+            Console.WriteLine(uniqChars);
+            
+        }
+    }
+}
---
+++ U2(Aggregate)/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace U2_Aggregate_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(Factorial(5));
+            CountExample();
+        }
+        static long Factorial(int number)
+        {
+            List<int> list = new List<int>();
+            for (int i = 1; i <= number; i++)
+            {
+                list.Add(i);
+            }
+
+            long result = list.Aggregate((x,y) => x*y);
+            return result;
+        }
+        static void CountExample()
+        {
+            var contacts = new List<Contact>()
+            {
+                new Contact() { Name = "Андрей", Phone = 79994500508 },
+                new Contact() { Name = "Сергей", Phone = 799990455 },
+                new Contact() { Name = "Иван", Phone = 79999675334 },
+                 new Contact() { Name = "Игорь", Phone = 8884994 },
+                 new Contact() { Name = "Анна", Phone = 665565656 },
+                 new Contact() { Name = "Василий", Phone = 3434 }
+            };
+            var result = contacts.Count(c => c.Phone.ToString().Length == 11 &&
+            c.Phone.ToString().StartsWith('7'));
+            Console.WriteLine(result);
+        }
+        class Contact
+        {
+            public string Name { get; set; }
+            public long Phone { get; set; }
+            
+        }
+    }
+}
