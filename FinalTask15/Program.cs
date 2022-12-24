1e495098362a173b400ee5211a61a23fa67b33fa
alexhrinyov <hrinyou29@gmail.com>
2022-12-24 17:52:00 +0300

    добавлено решение дз

---
+++ FinalTask15/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace FinalTask15
+{
+    //Напишите метод, который соберет всех учеников всех классов в один список, используя LINQ.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var classes = new[]
+            {
+               new Classroom { Students = {"Evgeniy", "Sergey", "Andrew"}, },
+               new Classroom { Students = {"Anna", "Viktor", "Vladimir"}, },
+               new Classroom { Students = {"Bulat", "Alex", "Galina"}, }
+           };
+            var allStudents = GetAllStudents(classes);
+
+            Console.WriteLine(string.Join(" ", allStudents));
+        }
+
+        static string[] GetAllStudents(Classroom[] classes)
+        {
+            string[]? students = classes.SelectMany(st => st.Students).ToArray();
+            return students;
+        }
+
+        public class Classroom
+        {
+            public List<string> Students = new List<string>();
+        }
+    }
+}
---
+++ U5(ToArray)/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace U5_ToArray_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> ints = new List<int>();
+            ints.Add(1);
+            ints.Add(2);
+            ints.Add(3);
+            ints.Add(4);
+            ints.Add(5);
+            // ToArray делает выполнение linq запроса мнгновенным, а не отложенным
+            var ints2 = ints.Where(i => i<3).OrderBy(i => i).ToArray();
+            ints.Add(0);
+            foreach (var item in ints2)
+            {
+                Console.WriteLine(item);
+            }
+        
+        }
+    }
+}
